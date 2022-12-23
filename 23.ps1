0e845286d2b69e42219719d940a5986d649efc59
Paul Sausage <luc.pitipuis@gmail.com>
2022-12-23 19:00:48 +0900

    cleaned up, streamlined a bit 23

--- 23.ps1
+++ 23.ps1
@@ -11,12 +11,10 @@ $sample = @'
 .#..#..
 '@ -split "`n"
 
-function Find-Result ($sample)
-{
+function Find-Result ($sample) {
     $elves = Parse-Input $sample
     $rounds = 10
-    for ($i = 0; $i -lt $rounds; $i++)
-    {
+    for ($i = 0; $i -lt $rounds; $i++) {
         $proposedChanges = Get-Phase1 $elves $i
         $null, $elves = Apply-Changes $proposedChanges
         #"end of round $i"
@@ -28,108 +26,80 @@ function Find-Result ($sample)
     ($maxY - $minY + 1) * ($maxX - $minX + 1) - $elves.Count
 }
 
-function Draw-Elves ($elves)
-{
+function Draw-Elves ($elves) {
     $maxY, $minY = $elves | ForEach-Object { $_[0] } | Measure-Object -Minimum -Maximum | ForEach-Object { $_.maximum, $_.minimum }
     $maxX, $minX = $elves | ForEach-Object { $_[1] } | Measure-Object -Minimum -Maximum | ForEach-Object { $_.maximum, $_.minimum }
-    for ([int]$i = $minY; $i -le $maxY; $i++)
-    {
-        for ([int]$j = $minX; $j -le $maxX; $j++)
-        {
+    for ([int]$i = $minY; $i -le $maxY; $i++) {
+        for ([int]$j = $minX; $j -le $maxX; $j++) {
             $pos = [tuple]::create($i, $j)
-            if ($elves.Contains($pos))
-            {
+            if ($elves.Contains($pos)) {
                 write-host '#' -NoNewline
             }
-            else
-            {
+            else {
                 write-host '.' -NoNewline
             }
         }
         write-host ''
     }
 }
-function Find-Result2 ($sample)
-{
+function Find-Result2 ($sample) {
 
     $elves = Parse-Input $sample
-    $rounds = 10
     $i = 0
-    do
-    {
+    do {
         $proposedChanges = Get-Phase1 $elves $i
-        $hasmoves, $elves = Apply-Changes $proposedChanges
+        $hasmoves, $elves = Apply-Changes $proposedChanges $elves
         $i++
+        Write-Host "end of round $i, $hasmoves move done"
     } while ($hasmoves)
-
     $i
 }
 
-function Apply-Changes ($proposedChanges)
-{
+function Apply-Changes ($proposedChanges, [System.Collections.ArrayList]$elves) {
     $conflicts = @(($proposedChanges.values | Group-Object | Where-Object { $_.count -gt 1 }) | ForEach-Object { $_.group[0] })
-    $newElves = @()
-    $moved = $false
-    foreach ($elf in $proposedChanges.GetEnumerator())
-    {
-        if ($conflicts.Contains($elf.Value))
-        {
-            $newElves += $elf.Key
-        }
-        else
-        {
-            $newElves += $elf.Value
-            if ($elf.Value -ne $elf.Key)
-            {
-                $moved = $true
-            }
-        }
+    $moved = 0
+    foreach ($elf in $proposedChanges.GetEnumerator().Where({ !$conflicts.Contains($_.Value) })) {
+        $elves.Remove($elf.Key)
+        $elves += $elf.Value
+        $moved++
     }
-    $moved, $newElves
+    $moved, $elves
 }
 
-function Get-Phase1 ($elves, $round)
-{
+function Get-Phase1 ($elves, $round) {
     $proposal = @(
         @(
-            @(-1, 0), @(-1, -1), @(-1, 1) # check north
+            [tuple]::Create(-1, 0), [tuple]::Create(-1, -1), [tuple]::Create(-1, 1) # check north
         ),
         @(
-            @(1, 0), @(1, -1), @(1, 1) # check south
+            [tuple]::Create(1, 0), [tuple]::Create(1, -1), [tuple]::Create(1, 1) # check south
         ),
         @(
-            @(0, -1), @(-1, -1), @(1, -1) # check west
+            [tuple]::Create(0, -1), [tuple]::Create(-1, -1), [tuple]::Create(1, -1) # check west
         ),
         @(
-            @(0, 1), @(-1, 1), @(1, 1) # check east
+            [tuple]::Create(0, 1), [tuple]::Create(-1, 1), [tuple]::Create(1, 1) # check east
         )
     )
-    $around = @(@(-1, -1), @(-1, 0), @(-1, 1), @(0, 1), @(1, 1), @(1, 0), @(1, -1), @(0, -1))
+    $around = @([tuple]::Create(-1, -1), [tuple]::Create(-1, 0), [tuple]::Create(-1, 1), [tuple]::Create(0, 1), [tuple]::Create(1, 1), [tuple]::Create(1, 0), [tuple]::Create(1, -1), [tuple]::Create(0, -1))
     $moves = @{}
-    foreach ($elf in $elves)
-    {
-        $moves[$elf] = $elf
+    foreach ($elf in $elves) {
         # if nothing around, don't move
-        if (!$around.where({ $elves.Contains([tuple]::Create(($elf[0] + $_[0]), ($elf[1] + $_[1]))) }))
-        {
+        $aroundValues = $around.where({ $elves.Contains([tuple]::Create(($elf[0] + $_[0]), ($elf[1] + $_[1]))) })
+        if (!$aroundValues) {
             continue
         }
 
-        for ($i = $round; $i -lt ($round + 4); $i++)
-        {
+        for ($i = $round; $i -lt ($round + 4); $i++) {
             $conflict = $false
             $prop = $proposal[$i % 4]
-            foreach ($p in $prop)
-            {
-                $newPos = [tuple]::Create(($elf[0] + $p[0]), ($elf[1] + $p[1]))
-                if ($elves.Contains($newPos))
-                {
+            foreach ($p in $prop) {
+                if ($aroundValues.Contains($p)) {
                     $conflict = $true
                     break
                 }
             }
-            if (!$conflict)
-            {
+            if (!$conflict) {
                 $moves[$elf] = [tuple]::Create(($elf[0] + $prop[0][0]), ($elf[1] + $prop[0][1]))
                 break
             }
@@ -138,19 +108,12 @@ function Get-Phase1 ($elves, $round)
     $moves
 }
 
-function Parse-Input ($sample)
-{
-    $elves = @()
-    for ($i = 0; $i -lt $sample.Count; $i++)
-    {
-        if ($sample[$i] -match '[#\.]+')
-        {
-            $map[$i] = @{}
-            for ($j = 0; $j -lt $sample[$i].Length; $j++)
-            {
-                
-                if ($sample[$i][$j] -eq '#')
-                {
+function Parse-Input ($sample) {
+    [System.Collections.ArrayList]$elves = @()
+    for ($i = 0; $i -lt $sample.Count; $i++) {
+        if ($sample[$i] -match '[#\.]+') {
+            for ($j = 0; $j -lt $sample[$i].Length; $j++) {
+                if ($sample[$i][$j] -eq '#') {
                     $elves += [tuple]::create([int]$i, [int]$j)
                 }
             }
@@ -167,5 +130,6 @@ function Parse-Input ($sample)
 
 'Second part, sample result should be: '
 Find-Result2 $sample
+
 
 Find-Result2 $data
